420dae71ecaf7e3a3482372693b2a633fcb97af5
ItsAcuallyAlex <104977689+ItsAcuallyAlex@users.noreply.github.com>
2022-06-27 18:15:21 +0200

    Grosse update
    Toutes les fonctions sont corrigées
    il me reste les conditions initiales à formuler correctement et je devrais être bon pour lancer les simulations

--- src/recent_minchin.py
+++ src/recent_minchin.py
@@ -1,37 +1,61 @@
 #### MODULES
+import scipy.optimize as scipy 
 
+import matplotlib.pyplot as plt
+import numpy as np
+import math
 #### MODULES
 
 
-
-#### CONDITIONS INITIALES
-
-#### CONDITIONS INITIALES
-
-
-
 #### CONSTANTES
-# Constantes pour la résistance
+# CONSTANTES PHOTOSYNTHESE __________
+# P c'est la durée d'éclairement
+P = 16 * 20
+"Durée d'éclairement en ??? pour 16h  de photopériode et 20°Cj"
+
+valeurs_photosynthese = np.array([429859.7489, 641689.2869, 237700.9616], dtype=float) 
+# CONSTANTES PHOTOSYNTHESE __________
+
+# CONSTANTES RESISTANCE __________
 temp_20 = 293
 gaz_p = 8.314
+# Viscosité selon Bancal et al. 2002 = 1M, on met en micromolaire ici
+viscosity = 1E6
 
-# Constantes pour le RER
-v_ = VALEUR_
-k_ = VALEUR_
+longueur_commune_entrenoeuds = np.array([2.91047327, 2.91047327, 2.91047327], dtype=float)
+rayon_commun_entrenoeuds = np.array([0.160215922,0.160215922, 0.160215922], dtype=float)
+"longueur et rayon moyen d'entrenoeud mature - COMMUNE"
+# CONSTANTES RESISTANCE __________
 
-# Pour l'utilisation
-delta = VALEUR_
-"Est-ce que delta sera bien une moyenne (donc une seule valeur par traitement) ?"
+# CONSTANTES UTILISATION ET VOLUME __________
+# CHECKER VOIR CE QUI EST CALCULE VIA LA FONCTION UTILISATION (INCREMENTALE DE VOLUME) COMPARER AVEC LA FONCTION QUE J'AI
+utilisation = np.array([236419.6269, 244842.0206, 186507.4465], dtype=float)
+"Utilisation à tBFV - HH LH LL"
 
-# Pour la variation de la masse en C
-surface_ = VALEUR_ 
-"J'ai oublié l'approche exacte pour la mesure"
+delta = np.array([1.03453E+13, 1.03453E+13, 1.03453E+13], dtype=float)
+"moyenne des delta à tBFV - HH LH LL - COMMUNE"
 
-# On garde un volume fixe moyen en regardant les organes qui ne sont plus en extension ?
-volume_fixe = VALEUR_
+# # On garde un volume fixe pour cette étape
+volume_fixe_feuilles = np.array([1.79192E-06, 1.79192E-06, 1.79192E-06], dtype=float)
+"Volume foliaire - COMMUNE"
 
+hauteur = 2E-3
+rayon = 1.25E-3
+# volume en m3
+formule = ((math.pi * rayon**2 * hauteur)/(3))
+volume_fixe_bourgeon = np.array([formule, formule, formule], dtype=float)
+"bourgeon assimilé à un cône - COMMUNE"
+# CONSTANTES UTILISATION ET VOLUME __________
 
+# CONSTANTES RER __________
+RER = np.array([110, 90, 90], dtype=float)
+"Valeurs de RER pour le puits 1 aka les organes en croissance"
 
+k = np.array([405.197, 397.561, 357.624], dtype=float)
+v = np.array([220, 180, 180], dtype=float)
+"Paramètres k et v calculés via cm_t0_puits1 - HH LH LL"
+"Résolu pour v = 2*RER"
+# CONSTANTES RER __________
 #### CONSTANTES
 
 
@@ -43,93 +67,127 @@ def PHOTOSYNTHESE_complete (alpha, PPFD, Ai_max, P) :
     eq = (alpha * PPFD * Ai_max)/(alpha * PPFD + Ai_max) * P 
 
     return eq
+def CONCENTRATION_SOURCE (surface_fixe_feuilles, volume_fixe_feuilles) :
+    "Variation de la concentration dans la source"
 
-
-def PHOTOSYNTHESE_simple (VALEUR_PHOTOSYNTHESE) :
-    """Equation de la photosynthèse, simplifiée"""
-
-    eq = VALEUR_PHOTOSYNTHESE 
+    #  voir a nouveau pour les ajouts
+    eq = PHOTOSYNTHESE_simple () * (surface_fixe_feuilles)/(volume_fixe_feuilles) 
 
     return eq
 
 
-def RESISTANCE_complete (viscosity, longueur_, rayon_) :
-    """Equation de la résistance au transport (Minchin et al. 1993), complète"""
+def PHOTOSYNTHESE_simple () :
+    "valeurs de photosynthèse à tBFV"
+
+    eq = valeurs_photosynthese
+
+    return eq
+
+
+def RESISTANCES (viscosity, longueur_commune_entrenoeuds, rayon_commun_entrenoeuds) :
+    "Equation de la résistance au transport (Minchin et al. 1993)"
 
     constante_resistance =  (8*viscosity)/(math.pi * temp_20 * gaz_p) 
 
-    eq = constante_resistance * (longueur_)/(rayon_**4)
+    eq = constante_resistance * (longueur_commune_entrenoeuds)/(rayon_commun_entrenoeuds**4)
+
+    eq = np.append(eq, (eq,eq))
+    "Pas correct pour la suite il faudra corriger l'output pour qu'il s'adapte aux valeurs variables entre conduits"
 
     return eq
 
 
-# Ci_m obtenu par la résolution du système à l'équilibre au pas de temps d'avant
-def INCREMENT_VOLUME_simple (Ci_m) :
-    """Equation pour incrément dvi_dt, simplifiée"""
+def FLUX_complet (Ci_m) :
+    "Equations de F01 et F02"
 
-    # 
-    eq = (v_ * Ci_m)/(k_ + Ci_m)
+    R_ = RESISTANCES (viscosity, longueur_commune_entrenoeuds, rayon_commun_entrenoeuds)
+
+    "R_0[0] = R_0 HH"
+    "R_0[1] = R_1 HH"
+    "R_0[2] = R_2 HH"
+
+    "Ci_m[0] = C0_m HH"
+    "Ci_m[1] = C1_m HH"
+    "Ci_m[2] = C2_m HH"
+
+    # a c'est un array pour correctement naviguer R_ 
+    a = np.array([0,3,6])
+    # besoin de déclarer eq pour la suite 
+    eq = np.empty(0)
+    for i in range(3) :
+        i = a[i]
+        denominateur = R_[1+i]*(R_[2+i]+R_[2+i])+R_[2+i]*R_[2+i]
+
+        F01 = Ci_m[0]*((R_[2+i]*(Ci_m[0]-Ci_m[1]) + R_[0+i]*(Ci_m[2]-Ci_m[1])) / denominateur)
+        F02 = Ci_m[0]*((R_[1+i]*(Ci_m[0]-Ci_m[2]) + R_[0+i]*(Ci_m[1]-Ci_m[2])) / denominateur)
+
+        eq = np.append(eq, (F01,F02))
 
     return eq
 
 
-def UTILISATION_simple (param) :
-    """Equation de l'utilisation, simplifiée"""
+def RERi (v, k, Ci_m) :
+    """Expression du RER"""
 
-    eq = delta * INCREMENT_VOLUME_simple (param)
+    eq = (v * Ci_m)/(k + Ci_m)
 
-    return eq 
-
-
-def FLUX_complet (param) :
-    """Equations solutionnées comme précédemment"""
-
-    R_ = RESISTANCE_complete (viscosity, longueur_, rayon_)
-
-    denominateur = R_[1]*(R_[2]+R_[3])+R_[2]*R_[3]
-
-    "Les x vont se jumeler avec C_0 pour n'avoir à la fin qu'un seul vecteur "
-    eq_0 = C_0*((R_[3]*(C_0-x_0[0]) + R_[1]*(x_0[1]-x_0[0])) / denominateur)
-    eq_1 = C_0*((R_[2]*(C_0-x_0[1]) + R_[1]*(x_0[0]-x_0[1])) / denominateur)
- 
-    eq = np.array([eq_0,eq_1])
+    return eq
 
 
-
-def VOLUME_simple (param) :
+def VOLUME_simple (Vi, Ci_m) :
     """Variation du volume des organes, simplifiée"""
 
-    eq = Vi * INCREMENT_VOLUME_simple (param)
+    eq = Vi * RERi (v, k, Ci_m)
     
     return eq
 
-def MASSE_MOBILE_simple (param) :
-    """Variation de la masse mobile en C dans un organe, simplifiée"""
 
-    eq = FLUX + PHOTOSYNTHESE_simple (VALEUR_PHOTOSYNTHESE) * surface_ - UTILISATION_simple (param)
+def UTILISATION_simple (delta, Vi) :
+    """Equation de l'utilisation, simplifiée"""
 
-    return eq
+    eq = delta * VOLUME_simple (Vi)
+
+    return eq 
 
 
-def CONCENTRATION_MOBILE_simple (param) : 
-    """Concentration en C mobile dans un organe sans la variation en volume, simplifiée"""
-
-    eq = (MASSE_MOBILE_simple (param)) / (volume_fixe) 
-
-    return eq
-
-def RESOLUTION_MINCHIN (param) :
+def A_RESOUDRE (Ci_m, Vi) :
     """equation à résoudre """
 
-    eq = FLUX + PHOTOSYNTHESE_simple (VALEUR_PHOTOSYNTHESE) - UTILISATION_simple (param)
+    C0_m = (PHOTOSYNTHESE_simple() * P) / Vi[0] - FLUX_complet (Ci_m)[0] - FLUX_complet (Ci_m)[2]
+    C1_m = FLUX_complet (Ci_m)[1] - UTILISATION_simple (delta, Vi)[0]
+    C2_m = FLUX_complet (Ci_m)[2] - UTILISATION_simple (delta, Vi)[1]
 
-    return eq
+    return [C0_m, C1_m, C2_m]
 #### FONCTIONS
 
 
+#### CONDITIONS INITIALES
+cm_t0_puits1 = np.array([405.197, 397.561, 357.624], dtype=float)
+"Concentrations mobiles en C à t0 - HH LH LL"
+
+cm_t0_puits2 = np.array([0, 0, 0], dtype=float)
+"Données à extraire de la publication Girault et al. 2008 - HH LH LL"
+#### CONDITIONS INITIALES
+
+"CONDITIONS INITIALES"
+# On a besoin des concentrations et des utilisations 
+Ci_m_t0 = np.array([405.197, 397.561, 357.624, 0, 0, 0], dtype=float)
+"Voir comment agencer cet array pour que la solution soit adaptée"
+
+
+"CONDITIONS INITIALES"
+
+
+
+
+
+
 
 #### RESOLUTION
-Cm_i = scipy.fsolve(RESOLUTION_MINCHIN, x0=x_0, args=(param), col_deriv=0, xtol=1.49012e-08, maxfev=0, band=None, epsfcn=None, factor=100, diag=None)
+# La résolution va nous donner les valeurs de Cm_i dans les puits et la source à t+1
+Cm_i = scipy.fsolve(A_RESOUDRE, x0=x_0, args=(param), col_deriv=0, xtol=1.49012e-08, maxfev=0, band=None, epsfcn=None, factor=100, diag=None)
+x_result = scipy.fsolve(equa, x0=x_0, args=(Vi), col_deriv=0, xtol=1.49012e-08, maxfev=0, band=None, epsfcn=None, factor=100, diag=None)
+
 #### RESOLUTION
 
 
@@ -228,172 +286,3 @@ Cm_i = scipy.fsolve(RESOLUTION_MINCHIN, x0=x_0, args=(param), col_deriv=0, xtol=
 
 
 
-
-
-
-
-
-
-
-#### ZONE DE TEST
-### TEXTE DE CONTEXTE
-# On veut créer une simulation où il y aura une source et deux puits :
-# Puits 1 = Tous les organes qui sont en croissance et qaui sont pas hyper interessants
-# Puits 2 = Les bourgeons axillaires Z2 et Z3  où l'on veut observer l'accumulation de C ainsi que la baisse de la croissance de l'axe primaire sous traitement LH 
-
-# But = Simuler les Cm dans les organes puits avec des valeurs fixées
-### TEXTE DE CONTEXTE
-
-
-### DEFINITION DE FONCTION
-## PHOTOSYNTHESE _________________________________________________________________________________________________________________________
-# On par sur des constantes si on a qu'un organe capable de photosynthèse
-alpha = 0
-Ai_max = 0
-# P = Durée d'éclairement journalière
-P = 0
-def nom_fonction_PHOTOSYNTHESE (PPFD, area) :
-    """Taux de production de photosynthétats"""
-    # Formule complète
-    photosynthese = (alpha * PPFD * Ai_max)/(alpha * PPFD + Ai_max) * P * area 
-
-    # Etape 1 = Constante 
-    photosynthese = VALEUR_A_AJOUTER
-
-    return photosynthese 
-## PHOTOSYNTHESE _________________________________________________________________________________________________________________________
-
-
-
-## RESISTANCE AU TRANSPORT ______________________________________________________________________________________________________
-# viscosité fixe ? 
-viscosity = VALEUR_A_AJOUTER 
-#  temp = température en K pour 20°C
-temp_20 = 293
-# R = constante des gaz parfaits
-gaz_p = 8.314
-# longueur_ = l'ensemble des longueurs entre les deux organes
-longueur_ = np.array([VALEUR_A_AJOUTER])
-# rayon_ = les rayons internes des conduits vasculaires
-rayon_ = np.array([VALEUR_A_AJOUTER])
-
-def nom_fonction_RESISTANCES (longueur_, rayon_) :
-    """Calcul du jeu de resistances au transport dans le modèle"""
-    constante_resistance =  (8*viscosity)/(math.pi * temp_20 * gaz_p) 
-    resistances = constante_resistance * (longueur_)/(rayon_**4)
-
-    # return un array de resistances 
-    return resistances
-## RESISTANCE AU TRANSPORT ______________________________________________________________________________________________________
-
-
- 
-## FLUX _________________________________________________________________________________________________________________________
-C_inconnues_t0 = np.array([VALEUR_A_AJOUTER]) 
-C_0 = 0
-# R_[1] = R_0
-# R_[2] = R_1
-# R_[3] = R_2
-R_ = nom_fonction_RESISTANCES (longueur_, rayon_)
-
-def nom_fonction_FLUX (C_inconnues, C_0) :
-    """Flux de C d'une source vers deux organes puits"""
-    delta = R_[1]*(R_[2]+R_[3])+R_[2]*R_[3]
-
-    # Sans doute pas la bonne forme car le modèle sera pas en râteau 
-    eq_0 = C_0*((R_[3]*(C_0-C_inconnues[0]) + R_[1]*(C_inconnues[1]-C_inconnues[0])) / delta)
-    eq_1 = C_0*((R_[2]*(C_0-C_inconnues[1]) + R_[1]*(C_inconnues[0]-C_inconnues[1])) / delta)
- 
-    eq = np.array([eq_0,eq_1])
-
-    return eq
-## FLUX _________________________________________________________________________________________________________________________
-
-
-
-## DILUTION Cm_ _________________________________________________________________________________________________________________
-def nom_fonction_DILUTION_Cm_ (parametres_maybe) :
-    """explications"""
-
-    eq = fonction_variation_masse / dVi_dt
-
-    return eq
-## DILUTION Cm_ _________________________________________________________________________________________________________________
-
-
-
-## UTILISATION POUR LA CROISSANCE _______________________________________________________________________________________________
-# # Il faut que la taille des arrays K_ et V_ soit identique à celle de l'array des solutions inconnues aka C_
-# K_ et V_ pas utilisées pour l'instant
-K_ = np.array([1,2])
-V_ = np.array([1,2])
-Cm_ = np.array([VALEUR_A_AJOUTER])
-delta = VALEUR_A_AJOUTER
-def nom_fonction_UTILISATION (parametres_maybe) :
-    """Utilisation du C pour la croissance des deux organes puits"""
-
-    # Etape 1 = Fitting sur données 
-    eq = np.array([VALEUR_A_AJOUTER])
-
-    # dVi_dt sera un array qu'on aura obtenu depuis la croissance en volume
-    eq = delta * dVi_dt
-
-    return eq
-## UTILISATION POUR LA CROISSANCE _______________________________________________________________________________________________
-
-
-
-## CROISSANCE EN VOLUME _________________________________________________________________________________________________________
-# V_t0 = Volume de l'organe à t0
-Vi_t0 = 0
-# RER sera un array obtenu par la data
-# Michaelienne pour le RER pour le moment, estimer "v et K" 
-RER = np.array([VALEUR_A_AJOUTER])
-# area sera un array obtenu par la data 
-area = np.array([VALEUR_A_AJOUTER])
-# Epsilon facteur de conversion obtenu par des mesures experimentales d'une feuille 
-epsilon = VALEUR_A_AJOUTER
-def nom_fonction_VOLUME_ORGANE (Volume, signaux, C) :
-    """Variation de l'utilisation en fonction du volume"""
-    Vi = epsilon * area
-    eq = Vi * RER 
-
-    return eq
-
-dVi_dt = scipy.fsolve(nom_fonction_VOLUME_ORGANE, x0=Vi_t0, args=(), col_deriv=0, xtol=1.49012e-08, maxfev=0, band=None, epsfcn=None, factor=100, diag=None)
-## CROISSANCE EN VOLUME _________________________________________________________________________________________________________
-
-
-
-## VARIATION CARBONE STOCK ______________________________________________________________________________________________________
-coeff_stockage = 0
-coeff_mobilisation = 0
-Cs_ = np.array([VALEUR_A_AJOUTER])
-Cm_ = np.array([VALEUR_A_AJOUTER])
-def nom_fonction_CARBONE_STOCKE (Cs_, Cm_) : 
-    """Variation du carbone de réserve"""
-    eq = coeff_stockage * Cm_ - coeff_mobilisation * Cs_
-
-    return  eq
-## VARIATION CARBONE STOCK ______________________________________________________________________________________________________
-
-
-
-## VARIATION MASSE EN C MOBILE __________________________________________________________________________________________________
-# gamma = respiration de maintenance
-gamma = 0
-# On le met à 0 pour cette étape
-EQUILIBRE_STOCK_MOBILE = 0
-def nom_fonction_MASSE_Cm_ (parametres_maybe) :
-    """Variation de la masse en C mobile au sein d'un organe"""
-    eq = FLUX + PHOTOSYNTHESE * SURFACE + VOLUME * (EQUILIBRE_STOCK_MOBILE) - UTILISATION - gamma
-
-    eq = FLUX + PHOTOSYNTHESE * SURFACE - UTILISATION
-
-    return eq 
-## VARIATION MASSE EN C MOBILE __________________________________________________________________________________________________
-
-
-
-### DEFINITION DE FONCTIONS
-#### ZONE DE TEST
