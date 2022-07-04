ae8f7afcdfed1499cd1f6d8598fd67eb92c75434
ItsAcuallyAlex <104977689+ItsAcuallyAlex@users.noreply.github.com>
2022-07-04 13:11:31 +0200

    .

--- src/modele_propre.py
+++ src/modele_propre.py
@@ -14,14 +14,18 @@ gaz_p = 8.314
 photosynthese_journaliere = np.array([429859.7489, 641689.2869, 237700.9616], dtype=float)
 
 longueur_entrenoeuds = np.array([2.91047327, 2.91047327, 2.91047327], dtype=float)
-rayon_entrenoeuds = np.array([0.160215922,0.160215922, 0.160215922], dtype=float)/100
+rayon_entrenoeuds = np.array([2E-4, 2E-4, 2E-4], dtype=float)
+# Valeurs en cm
 
 volumes_puits1 = np.array([1.79192E-06, 1.79192E-06, 1.79192E-06], dtype=float)
 
-C0_m_t0_Sucrose_fitted = np.array([360,300,300], dtype=float)
-C0_m_t0_Sucrose_moyen = np.array([327.2154541, 327.2154541, 327.2154541], dtype=float)
-C1_m_t0 = np.array([518.3472222, 479.0671296, 479.0671296], dtype=float)
-C2_m_t0 = np.array([0.069444444, 0.069444444, 0.069444444], dtype=float)
+# C0_m = np.array([360,300,300], dtype=float)
+# Fitté
+C0_m = np.array([327.2154541, 327.2154541, 327.2154541], dtype=float)
+# Identique
+
+C1_m = np.array([518.3472222, 479.0671296, 479.0671296], dtype=float)
+C2_m = np.array([0.069444444, 0.069444444, 0.069444444], dtype=float)
 
 k = np.array([405.197, 397.561, 357.624], dtype=float)
 v = np.array([220, 180, 180], dtype=float)
@@ -32,32 +36,62 @@ delta_masse_volume = np.array([1.03453E+13, 1.03453E+13, 1.03453E+13], dtype=flo
 #### FONCTIONS DU MODELE
 def PHOTOSYNTHESE_SOURCE () :
     return photosynthese_journaliere
+"PAS UTILISE POUR L'INSTANT"
 
+# def RESISTANCES_TRANSPORT (longueur_entrenoeuds, rayon_entrenoeuds) :
+    # constante_resistance =  (8*viscosity)/(math.pi * temp_20 * gaz_p)
 
-def RESISTANCES_TRANSPORT (longueur_entrenoeuds, rayon_entrenoeuds) :
-    constante_resistance =  (8*viscosity)/(math.pi * temp_20 * gaz_p) 
+    # resistances_modele = constante_resistance * (longueur_entrenoeuds)/(rayon_entrenoeuds**4)
 
-    resistances_modele = constante_resistance * (longueur_entrenoeuds)/(rayon_entrenoeuds**4)
-
-    resistances_modele = np.append(resistances_modele, (resistances_modele, resistances_modele))
+    # resistances_modele = np.append(resistances_modele, (resistances_modele, resistances_modele))
     
-    return resistances_modele 
+    # return resistances_modele 
+    
+def RESISTANCES_TRANSPORT (longueur_entrenoeuds, rayon_entrenoeuds) :
+    constante_resistance =  (8*viscosity)/(math.pi * temp_20 * gaz_p)
 
+
+    resistances_modele = constante_resistance * ((longueur_entrenoeuds)/(rayon_entrenoeuds**4))
+
+    print("BBBB", resistances_modele)
+    resistances_modele = np.append(resistances_modele, (resistances_modele, resistances_modele))
+    print("BBBB", resistances_modele)
+    return resistances_modele
+
+R_ = RESISTANCES_TRANSPORT (longueur_entrenoeuds, rayon_entrenoeuds)
+print("Longueur R_",len(R_),R_)
 
 def FLUX_CARBONE (longueur_entrenoeuds, rayon_entrenoeuds, C0_m, C1_m, C2_m) :
     R_ = RESISTANCES_TRANSPORT (longueur_entrenoeuds, rayon_entrenoeuds)
 
-    denominateur_delta = R_[0]*(R_[1] + R_[2]) + R_[1]* R_[2]
-
+    denominateur_delta = R_[0]*(R_[1] + R_[2]) + R_[1]*R_[2]
+    print("DDD", denominateur_delta)
+    # print("Longueur dénominateur_delta",len(denominateur_delta), denominateur_delta)
+    
     F01 = (R_[2]*(C0_m - C1_m) + R_[0]*(C2_m - C1_m)) / denominateur_delta
     F02 = (R_[1]*(C0_m - C1_m) + R_[0]*(C1_m - C2_m)) / denominateur_delta
+    print("CCC", F01)
 
-    return [F01, F02]
-    
 
-def VOLUME_PUITS (volumes_puits1) :
-    return volumes_puits1 
 
+    FLUX = np.append(F01, F02)
+
+    return FLUX
+
+# def FLUX_CARBONE (longueur_entrenoeuds, rayon_entrenoeuds, C0_m, C1_m, C2_m) :
+#     R_ = RESISTANCES_TRANSPORT (longueur_entrenoeuds, rayon_entrenoeuds)
+
+#     denominateur_delta = R_[0]*(R_[1] + R_[2]) + R_[1]*R_[2]
+
+#     F01 = (R_[2]*(C0_m - C1_m) + R_[0]*(C2_m - C1_m)) / denominateur_delta
+#     F02 = (R_[1]*(C0_m - C1_m) + R_[0]*(C1_m - C2_m)) / denominateur_delta
+
+#     FLUX = np.append(F01, F02)
+
+#     return FLUX
+
+# def VOLUME_PUITS (volumes_puits1) :
+#     return volumes_puits1 
 
 def RER_PUITS (v, k, C1_m, C2_m) :
 
@@ -65,60 +99,66 @@ def RER_PUITS (v, k, C1_m, C2_m) :
     RER_puits2 = (v*C2_m)/(k+C2_m)
 
     return [RER_puits1, RER_puits2]
+"PAS UTILISE POUR L'INSTANT"
 
+def UTILISATION_CARBONE (delta_masse_volume) :
 
-def UTILISATION_CARBONE (volumes_puits1, delta_masse_volume) :
-
-    utilisation_puits1 = delta_masse_volume * VOLUME_PUITS(volumes_puits1)
+    utilisation_puits1 = delta_masse_volume * volumes_puits1
     utilisation_puits2 = utilisation_puits1/100
 
-    return [utilisation_puits1, utilisation_puits2]
+    UTILISATIONS = np.append(utilisation_puits1, utilisation_puits2)
+
+    return UTILISATIONS
+
+def A_RESOUDRE (longueur_entrenoeuds, rayon_entrenoeuds, C0_m, C1_m, C2_m, delta_masse_volume) :
+
+    C1_m_equilibre = C0_m*FLUX_CARBONE(longueur_entrenoeuds, rayon_entrenoeuds, C0_m, C1_m, C2_m) - UTILISATION_CARBONE(delta_masse_volume)
+    C2_m_equilibre = C0_m*FLUX_CARBONE(longueur_entrenoeuds, rayon_entrenoeuds, C0_m, C1_m, C2_m) - UTILISATION_CARBONE(delta_masse_volume)
+
+    EQUILIBRE_SOLUTIONS_PUITS = np.append(C1_m_equilibre, C2_m_equilibre)
+
+    return EQUILIBRE_SOLUTIONS_PUITS
+#### FONCTIONS DU MODELE   
 
 
-def A_RESOUDRE (longueur_entrenoeuds, rayon_entrenoeuds, C0_m, C1_m, C2_m, delta_masse_volume, volumes_puits1) :
+# print("resistances : ", RESISTANCES_TRANSPORT (longueur_entrenoeuds, rayon_entrenoeuds))
+# print("flux : ", FLUX_CARBONE (longueur_entrenoeuds, rayon_entrenoeuds, C0_m, C1_m, C2_m))
+# print("utilisation : " ,UTILISATION_CARBONE (delta_masse_volume))
 
-    C1_m_equilibre = C0_m*FLUX_CARBONE(longueur_entrenoeuds, rayon_entrenoeuds, C0_m, C1_m, C2_m) - UTILISATION_CARBONE(delta_masse_volume, volumes_puits1)
-    C2_m_equilibre = C0_m*FLUX_CARBONE(longueur_entrenoeuds, rayon_entrenoeuds, C0_m, C1_m, C2_m) - UTILISATION_CARBONE(delta_masse_volume, volumes_puits1)
-    return [C1_m_equilibre, C2_m_equilibre]
+
+A = np.array([longueur_entrenoeuds])
+B = np.array([rayon_entrenoeuds])
+C = np.array([C0_m])
+X = np.array([C1_m])
+Y = np.array([C2_m])
+D = np.array([volumes_puits1])
+E = np.array([delta_masse_volume])
     
-
-# print(UTILISATION_CARBONE(delta_masse_volume, volumes_puits1))
+X0 = np.append(X, Y)
+args = A,B,C,D,E
     
-# print(A_RESOUDRE (longueur_entrenoeuds, rayon_entrenoeuds, C0_m_t0_Sucrose_fitted, C1_m_t0 , C2_m_t0, delta_masse_volume, volumes_puits1))
+equilibre_concentrations = scipy.fsolve(A_RESOUDRE, x0=(X, Y), args=(args))
 
-for condition in range(3) :
-    # = np.array([photosynthese_journaliere[condition]])
-    A = np.array([longueur_entrenoeuds[condition]])
-    B = np.array([rayon_entrenoeuds[condition]])
-    # C0_m_t0_Sucrose_fitted = np.array([C0_m_t0_Sucrose_fitted[condition]])
-    C = np.array([C0_m_t0_Sucrose_moyen[condition]])
-    X = np.array([C1_m_t0[condition]])
-    Y = np.array([C2_m_t0[condition]])
-    D = np.array([volumes_puits1[condition]])
-    # = np.array([k[condition]])
-    # = np.array([v[condition]])
-    E = np.array([delta_masse_volume[condition]])
+print("FONCTION PASSEE")
+
+
+
+
+# for condition in range(3) :
+#     # = np.array([photosynthese_journaliere[condition]])
+#     A = np.array([longueur_entrenoeuds[condition]])
+#     B = np.array([rayon_entrenoeuds[condition]])
+#     # C0_m_t0_Sucrose_fitted = np.array([C0_m_t0_Sucrose_fitted[condition]])
+#     C = np.array([C0_m[condition]])
+#     X = np.array([C1_m[condition]])
+#     Y = np.array([C2_m[condition]])
+#     D = np.array([volumes_puits1[condition]])
+#     # = np.array([k[condition]])
+#     # = np.array([v[condition]])
+#     E = np.array([delta_masse_volume[condition]])
     
-    # X0 = np.append(X, Y)
-    args = A,B,C,D,E
+#     X0 = np.append(X, Y)
+#     args = A,B,C,D,E
     
-    equilibre_concentrations = scipy.fsolve(A_RESOUDRE, x0=(X, Y), args=(args))
+#     equilibre_concentrations = scipy.fsolve(A_RESOUDRE, x0=(X, Y), args=(args))
 
-
-
-
-
-
-    # photosynthese_journaliere = np.array([429859.7489, 641689.2869, 237700.9616], dtype=float)
-    longueur_entrenoeuds = np.array([2.91047327, 2.91047327, 2.91047327], dtype=float)
-    rayon_entrenoeuds = np.array([0.160215922,0.160215922, 0.160215922], dtype=float)/100
-    volumes_puits1 = np.array([1.79192E-06, 1.79192E-06, 1.79192E-06], dtype=float)
-    C0_m_t0_Sucrose_fitted = np.array([360,300,300], dtype=float)
-    C0_m_t0_Sucrose_moyen = np.array([327.2154541, 327.2154541, 327.2154541], dtype=float)
-    C1_m_t0 = np.array([518.3472222, 479.0671296, 479.0671296], dtype=float)
-    C2_m_t0 = np.array([0.069444444, 0.069444444, 0.069444444], dtype=float)
-    # k = np.array([405.197, 397.561, 357.624], dtype=float)
-    # v = np.array([220, 180, 180], dtype=float)
-    delta_masse_volume = np.array([1.03453E+13, 1.03453E+13, 1.03453E+13], dtype=float) 
-
-#### FONCTIONS DU MODELE
--- src/pas_2_cervo.py
+++ src/pas_2_cervo.py
@@ -7,97 +7,121 @@ import numpy as np
 import math
 #### MODULES
 
-
-
-
-valeurs_photosynthese = np.array([429859.7489], dtype=float) 
-def PHOTOSYNTHESE_simple () :
-
-    eq = valeurs_photosynthese
-
-    return eq
-
-
-
+#### CONSTANTES VRAIMENT CONSTANTES
 temp_20 = 293
 gaz_p = 8.314
 viscosity = 1E6
-longueur_commune_entrenoeuds = np.array([2.91047327], dtype=float)
-rayon_commun_entrenoeuds = np.array([0.160215922], dtype=float)/100
-def RESISTANCES () :
+hauteur_bourgeon = 2E-3
+rayon_bourgeon = 1.25E-3
+#### CONSTANTES VRAIMENT CONSTANTES
 
+#### FORMULES
+formule_cone = ((math.pi * rayon_bourgeon**2 * hauteur_bourgeon)/(3))
+#### FORMULES
+
+#### VARIABLES ENTRE CONDITIONS
+BASE_valeur_photosynthese = np.array([429859.7489], dtype=float)
+BASE_longueur_commune_entrenoeuds = np.array([2.91047327E-2], dtype=float)
+BASE_rayon_commun_entrenoeuds = np.array([5.1E-6], dtype=float)
+BASE_concentration_0 = np.array([360], dtype=float)
+BASE_valeurs_RER_bourgeon = np.array([110], dtype=float)
+BASE_valeurs_RER_feuilles = np.array([110], dtype=float)
+BASE_volume_fixe_bourgeon = np.array([formule_cone], dtype=float)
+BASE_volume_fixe_feuilles = np.array([1.79192E-06], dtype=float)
+BASE_coef_delta_feuilles = np.array([1.03453E+13], dtype=float)
+BASE_coef_delta_bourgeon = np.array([1.03453E+13], dtype=float)
+
+conditions_ini_C1C2 = np.array([518.3472222, 0.069444444], dtype=float)
+#### VARIABLES ENTRE CONDITIONS
+
+#### FONCTIONS
+def PHOTOSYNTHESE_simple (valeur_photosynthese) :
+
+    eq = valeur_photosynthese
+
+    return eq
+print("Valeurs photosynthèse : ", PHOTOSYNTHESE_simple (BASE_valeur_photosynthese))
+
+def RESISTANCES (longueur_commune_entrenoeuds, rayon_commun_entrenoeuds) :
+
+    # Particule qui ne varie pas
     constante_resistance =  (8*viscosity)/(math.pi * temp_20 * gaz_p) 
 
+    # calcul de la résistance
     eq = constante_resistance * (longueur_commune_entrenoeuds)/(rayon_commun_entrenoeuds**4)
 
+    # 3 fois la même valeur R0 R1 R2
     eq = np.append(eq, (eq,eq))
 
     return eq
-"PAS DE DIFFERENCE REISSTANCE"
+print("Valeurs resistances : ", RESISTANCES (BASE_longueur_commune_entrenoeuds, BASE_rayon_commun_entrenoeuds))
 
-C_0 = 360
-def FLUX_2puits (Ci_m) :
+def FLUX_2puits (concentration_0, Ci_m, longueur_commune_entrenoeuds, rayon_commun_entrenoeuds) :
 
-    eq = np.empty(0)
-    R_ = RESISTANCES ()
+    R_ = RESISTANCES (longueur_commune_entrenoeuds, rayon_commun_entrenoeuds)
     denominateur = R_[0]*(R_[1]+R_[2])+R_[1]*R_[2]
 
-    F01 = C_0*((R_[2]*(C_0-Ci_m[0]) + R_[0]*(Ci_m[1]-Ci_m[0])) / denominateur)
-    F02 = C_0*((R_[1]*(C_0-Ci_m[1]) + R_[0]*(Ci_m[0]-Ci_m[1])) / denominateur)
+    F01 = concentration_0*((R_[2]*(concentration_0-Ci_m[0]) + R_[0]*(Ci_m[1]-Ci_m[0])) / denominateur)
+    F02 = concentration_0*((R_[1]*(concentration_0-Ci_m[1]) + R_[0]*(Ci_m[0]-Ci_m[1])) / denominateur)
 
-    eq = np.append(eq, (F01,F02))
+    return F01, F02
+print("Valeurs flux : ", FLUX_2puits (BASE_concentration_0, conditions_ini_C1C2, BASE_longueur_commune_entrenoeuds, BASE_rayon_commun_entrenoeuds))
 
-    return eq
+def VOLUME (volume_fixe_feuilles, volume_fixe_bourgeon) :
 
-
-k = np.array([405.197,405.197], dtype=float)
-v = np.array([220,220], dtype=float)
-def RER (Ci_m) :
-    eq = np.empty(0)
-
-    for i in range(2) :
-        RERi = (v[i] * Ci_m[0+i])/(k[i] + Ci_m[0+i])
-        eq = np.append(eq, RERi)
-
-    b = np.array([eq/10], dtype=float)
-    "b c'est les RER pour le puits 2"
+    volume_feuilles = volume_fixe_feuilles
+    volume_bourgeon = volume_fixe_bourgeon
     
-    return eq
-"PAS DE DIFFERENCE RER"
+    return volume_feuilles, volume_bourgeon
+print("Valeurs volume : ", VOLUME (BASE_volume_fixe_feuilles, BASE_volume_fixe_bourgeon))
 
-hauteur = 2E-3
-rayon = 1.25E-3
-formule = ((math.pi * rayon**2 * hauteur)/(3))
-volume_fixe_bourgeon = np.array([formule], dtype=float)
-volume_fixe_feuilles = np.array([1.79192E-06], dtype=float)
-def VOLUME () :
-
-    eq = np.append(volume_fixe_feuilles, volume_fixe_bourgeon)
+def RER (valeurs_RER_bourgeon, valeurs_RER_feuilles) :
     
-    return eq
-"DIFFERENCE VOLUME"
+    RER1 = valeurs_RER_feuilles
+    RER2 = valeurs_RER_bourgeon
 
-delta = np.array([1.03453E+13, 1.03453E+13], dtype=float)
-def UTILISATION () :
-    eq = delta * VOLUME ()
+    return RER1, RER2
+print("Valeurs RER : ", RER (BASE_valeurs_RER_bourgeon, BASE_valeurs_RER_feuilles))
 
-    eq = np.append(eq, eq/50)
+def COEFF_delta (coefficient_delta_feuilles, coefficient_delta_bourgeon) :
+    
+    delta_feuilles = coefficient_delta_feuilles
+    delta_bourgeon = coefficient_delta_bourgeon
 
-    return eq 
-"DIFFERENCE UTILISATION"
+    return delta_feuilles, delta_bourgeon
+print("Valeurs delta : ", COEFF_delta (BASE_coef_delta_feuilles, BASE_coef_delta_bourgeon))
 
+def UTILISATION (coef_delta_feuilles, coef_delta_bourgeon, volume_fixe_feuilles, volume_fixe_bourgeon, valeurs_RER_bourgeon, valeurs_RER_feuilles) :
+    
+    U1 = COEFF_delta (coef_delta_feuilles, coef_delta_bourgeon)[0] * VOLUME (volume_fixe_feuilles, volume_fixe_bourgeon)[0] * RER (valeurs_RER_bourgeon, valeurs_RER_feuilles)[0]
+    U2 = COEFF_delta (coef_delta_feuilles, coef_delta_bourgeon)[1] * VOLUME (volume_fixe_feuilles, volume_fixe_bourgeon)[1] * RER (valeurs_RER_bourgeon, valeurs_RER_feuilles)[1]
 
-def A_RESOUDRE (Ci_m) :
+    return U1, U2
+print("Valeurs utilisation : ", UTILISATION (BASE_coef_delta_feuilles, BASE_coef_delta_bourgeon, BASE_volume_fixe_feuilles, BASE_volume_fixe_bourgeon, BASE_valeurs_RER_bourgeon, BASE_valeurs_RER_feuilles))
 
-    eq = np.empty(0)
+def A_RESOUDRE (concentration_0, Ci_m, longueur_commune_entrenoeuds, rayon_commun_entrenoeuds, coef_delta_feuilles, coef_delta_bourgeon, volume_fixe_feuilles, volume_fixe_bourgeon, valeurs_RER_bourgeon, valeurs_RER_feuilles) :
 
-    C1_m = FLUX_2puits (Ci_m)[0] - UTILISATION ()[0]
-    C2_m = FLUX_2puits (Ci_m)[1] - UTILISATION ()[1]
+    C1_m = FLUX_2puits (concentration_0, Ci_m, longueur_commune_entrenoeuds, rayon_commun_entrenoeuds)[0] - UTILISATION (coef_delta_feuilles, coef_delta_bourgeon, volume_fixe_feuilles, volume_fixe_bourgeon, valeurs_RER_bourgeon, valeurs_RER_feuilles)[0]
+    C2_m = FLUX_2puits (concentration_0, Ci_m, longueur_commune_entrenoeuds, rayon_commun_entrenoeuds)[1] - UTILISATION (coef_delta_feuilles, coef_delta_bourgeon, volume_fixe_feuilles, volume_fixe_bourgeon, valeurs_RER_bourgeon, valeurs_RER_feuilles)[1]
 
-    return [C1_m, C2_m]
+    return C1_m, C2_m
+#### FONCTIONS
 
+# condition[0] = HH
+# condition[1] = LH
+# condition[2] = LL
 
-conditions_ini = np.array([518.3472222, 0.069444444], dtype=float)
+for condition in range(1) : 
+    longueur_commune_entrenoeuds = BASE_longueur_commune_entrenoeuds[condition]
+    rayon_commun_entrenoeuds = BASE_rayon_commun_entrenoeuds[condition]
+    valeur_photosynthese = BASE_valeur_photosynthese[condition]
+    concentration_0 = BASE_concentration_0[condition]
+    coef_delta_feuilles = BASE_coef_delta_feuilles[condition]
+    coef_delta_bourgeon = BASE_coef_delta_bourgeon[condition]
+    volume_fixe_feuilles = BASE_volume_fixe_feuilles[condition]
+    volume_fixe_bourgeon = BASE_volume_fixe_bourgeon[condition]
+    valeurs_RER_bourgeon = BASE_valeurs_RER_bourgeon[condition]
+    valeurs_RER_feuilles = BASE_valeurs_RER_feuilles[condition]
 
-
-Ci_m = scipy.fsolve(A_RESOUDRE, x0=conditions_ini, args=(), col_deriv=0, xtol=1.49012e-08, maxfev=0, band=None, epsfcn=None, factor=100, diag=None) 
+    # Ci_m = scipy.fsolve(A_RESOUDRE, x0=conditions_ini, args=(concentration_0, longueur_commune_entrenoeuds, rayon_commun_entrenoeuds, coef_delta_feuilles, coef_delta_bourgeon, volume_fixe_feuilles, volume_fixe_bourgeon, valeurs_RER_bourgeon, valeurs_RER_feuilles), col_deriv=0, xtol=1.49012e-08, maxfev=0, band=None, epsfcn=None, factor=100, diag=None) 
+    # print(Ci_m)
