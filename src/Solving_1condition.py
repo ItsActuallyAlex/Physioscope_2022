2cd4dc2a38086c7970fef796c0cdc59121659b3c
ItsAcuallyAlex <104977689+ItsAcuallyAlex@users.noreply.github.com>
2022-07-05 11:18:15 +0200

    .

--- src/Solving_1condition.py
+++ src/Solving_1condition.py
@@ -13,6 +13,8 @@ gaz_p = 8.314
 viscosity = 1E6
 hauteur_bourgeon = 2E-3
 rayon_bourgeon = 1.25E-3
+
+nom_conditions = np.array(["HH", "LH", "LL"], dtype=str)
 #### CONSTANTES VRAIMENT CONSTANTES
 
 #### FORMULES
@@ -36,7 +38,7 @@ formule_cone = ((math.pi * rayon_bourgeon**2 * hauteur_bourgeon)/(3))
 
 BASE_valeur_photosynthese = np.array([429859.7489, 641689.2869, 237700.9616], dtype=float)
 BASE_longueur_commune_entrenoeuds = np.array([0.02732, 0.02732, 0.02732], dtype=float)
-BASE_rayon_commun_entrenoeuds = np.array([5.1E-6, 5.1E-6, 5.1E-6], dtype=float)
+BASE_rayon_commun_entrenoeuds = np.array([35.1E-6, 35.1E-6, 35.1E-6], dtype=float)
 # Valeur moyenne de rayon Minchin
 BASE_C_0 = np.array([360, 300, 300], dtype=float)
 BASE_valeurs_RER_bourgeon = np.array([110, 90, 90], dtype=float)
@@ -46,18 +48,18 @@ BASE_volume_fixe_feuilles = np.array([1.79192E-06, 1.79192E-06, 1.79192E-06], dt
 BASE_coef_delta_feuilles = np.array([1.03453E+13, 1.03453E+13, 1.03453E+13], dtype=float)
 BASE_coef_delta_bourgeon = np.array([1.03453E+13, 1.03453E+13, 1.03453E+13], dtype=float)
 
-BASE_conditions_ini_C1C2 = np.array([518.3472222, 0.069444444, 479.0671296, 0.069444444, 479.0671296, 0.069444444], dtype=float)
-# conc_ini_C1 = np.array([518.3472222, 479.0671296, 479.0671296], dtype=float)
-# conc_ini_C2 = np.array([0.069444444, 0.069444444, 0.069444444], dtype=float)
+BASE_k1 = np.array([405.197, 397.561, 357.624], dtype=float)
+BASE_v1 = np.array([220, 180, 180], dtype=float)
+BASE_k2 = np.array([405.197, 397.561, 357.624], dtype=float)
+BASE_v2 = np.array([220, 180, 180], dtype=float)
+# BASE_conditions_ini_C1C2_équivalentCARBONE = np.array([7.775208333, , 7.186006944, , 7.186006944], dtype=float) 
+
+# BASE_conditions_ini_C1C2 = np.array([518.3472222, 0.069444444, 479.0671296, 0.069444444, 479.0671296, 0.069444444], dtype=float)
+BASE_conc_ini_C1 = np.array([518.3472222, 479.0671296, 479.0671296], dtype=float)
+BASE_conc_ini_C2 = np.array([0.069444444, 0.069444444, 0.069444444], dtype=float)
 #### VARIABLES ENTRE CONDITIONS
 
 #### FONCTIONS
-def PHOTOSYNTHESE_simple (valeur_photosynthese) :
-
-    eq = valeur_photosynthese
-
-    return eq
-print("Valeurs photosynthèse : ", PHOTOSYNTHESE_simple (BASE_valeur_photosynthese))
 
 def RESISTANCES (longueur_commune_entrenoeuds, rayon_commun_entrenoeuds) :
 
@@ -73,10 +75,7 @@ def RESISTANCES (longueur_commune_entrenoeuds, rayon_commun_entrenoeuds) :
     return eq
 print("Valeurs resistances : ", RESISTANCES (BASE_longueur_commune_entrenoeuds, BASE_rayon_commun_entrenoeuds))
 
-def FLUX_2puits (Ci_m, C_0, longueur_commune_entrenoeuds, rayon_commun_entrenoeuds) :
-
-    C1_m = Ci_m[0]
-    C2_m = Ci_m[1]
+def FLUX_2puits (C1_m, C2_m, C0_m, longueur_commune_entrenoeuds, rayon_commun_entrenoeuds) :
 
     R_ = RESISTANCES (longueur_commune_entrenoeuds, rayon_commun_entrenoeuds)
     R_0 = R_[0]
@@ -84,60 +83,39 @@ def FLUX_2puits (Ci_m, C_0, longueur_commune_entrenoeuds, rayon_commun_entrenoeu
     R_2 = R_[2]
     denominateur = R_0*(R_1+R_2)+R_1*R_2
 
-    F01 = C_0*((R_2*(C_0-C1_m) + R_0*(C2_m-C1_m)) / denominateur)
-    F02 = C_0*((R_1*(C_0-C2_m) + R_0*(C1_m-C2_m)) / denominateur)
+    F01 = ((R_2*(C0_m-C1_m) + R_0*(C2_m-C1_m)) / denominateur)
+    F02 = ((R_1*(C0_m-C2_m) + R_0*(C1_m-C2_m)) / denominateur)
 
     eq = np.append(F01,F02)
 
     return eq
-print("Valeurs flux : ", FLUX_2puits (BASE_conditions_ini_C1C2, BASE_C_0, BASE_longueur_commune_entrenoeuds, BASE_rayon_commun_entrenoeuds))
+# print("Valeurs flux : ", FLUX_2puits (BASE_conditions_ini_C1C2, BASE_C_0, BASE_longueur_commune_entrenoeuds, BASE_rayon_commun_entrenoeuds))
 
-def VOLUME (volume_fixe_feuilles, volume_fixe_bourgeon) :
-
-    volume_feuilles = volume_fixe_feuilles
-    volume_bourgeon = volume_fixe_bourgeon
-
-    eq = np.append(volume_feuilles, volume_bourgeon)
+def RER (C1_m, C2_m, v_1, k_1, v_2, k_2) :
     
-    return eq
-print("Valeurs volume : ", VOLUME (BASE_volume_fixe_feuilles, BASE_volume_fixe_bourgeon))
+    RER1 = (v_1*C1_m)/(k_1+C1_m)
+    RER2 = (v_2*C2_m)/(k_2+C2_m)
 
-def RER (valeurs_RER_bourgeon, valeurs_RER_feuilles) :
-    
-    RER1 = valeurs_RER_feuilles
-    RER2 = valeurs_RER_bourgeon
-
-    eq = np.append(RER1, RER2)
+    eq = np.append([RER1, RER2], dtype=float)
 
     return eq
-print("Valeurs RER : ", RER (BASE_valeurs_RER_bourgeon, BASE_valeurs_RER_feuilles))
+# print("Valeurs RER : ", RER (BASE_valeurs_RER_bourgeon, BASE_valeurs_RER_feuilles))
 
-def COEFF_delta (coefficient_delta_feuilles, coefficient_delta_bourgeon) :
+def UTILISATION (coef_delta_feuilles, coef_delta_bourgeon, volume_fixe_feuilles, volume_fixe_bourgeon, C1_m, C2_m, v_1, k_1, v_2, k_2) :
     
-    delta_feuilles = coefficient_delta_feuilles
-    delta_bourgeon = coefficient_delta_bourgeon
-
-    eq = np.append(delta_feuilles, delta_bourgeon)
-
-    return eq
-print("Valeurs delta : ", COEFF_delta (BASE_coef_delta_feuilles, BASE_coef_delta_bourgeon))
-
-def UTILISATION (coef_delta_feuilles, coef_delta_bourgeon, volume_fixe_feuilles, volume_fixe_bourgeon, valeurs_RER_bourgeon, valeurs_RER_feuilles) :
-    
-    U1 = COEFF_delta (coef_delta_feuilles, coef_delta_bourgeon)[0] * VOLUME (volume_fixe_feuilles, volume_fixe_bourgeon)[0] * RER (valeurs_RER_bourgeon, valeurs_RER_feuilles)[0]
-    U2 = COEFF_delta (coef_delta_feuilles, coef_delta_bourgeon)[1] * VOLUME (volume_fixe_feuilles, volume_fixe_bourgeon)[1] * RER (valeurs_RER_bourgeon, valeurs_RER_feuilles)[1]
+    U1 = coef_delta_feuilles * volume_fixe_feuilles * RER (C1_m, C2_m, v_1, k_1, v_2, k_2)[0]
+    U2 = coef_delta_bourgeon * volume_fixe_bourgeon * RER (C1_m, C2_m, v_1, k_1, v_2, k_2)[1]
 
     eq = np.append(U1, U2)
 
     return eq
-print("Valeurs utilisation : ", UTILISATION (BASE_coef_delta_feuilles, BASE_coef_delta_bourgeon, BASE_volume_fixe_feuilles, BASE_volume_fixe_bourgeon, BASE_valeurs_RER_bourgeon, BASE_valeurs_RER_feuilles))
+# print("Valeurs utilisation : ", UTILISATION (BASE_coef_delta_feuilles, BASE_coef_delta_bourgeon, BASE_volume_fixe_feuilles, BASE_volume_fixe_bourgeon, BASE_valeurs_RER_bourgeon, BASE_valeurs_RER_feuilles))
 
-def A_RESOUDRE (Ci_m, C_0, longueur_commune_entrenoeuds, rayon_commun_entrenoeuds, coef_delta_feuilles, coef_delta_bourgeon, volume_fixe_feuilles, volume_fixe_bourgeon, valeurs_RER_bourgeon, valeurs_RER_feuilles) :
+def A_RESOUDRE (C1_m, C2_m, C0_m, longueur_commune_entrenoeuds, rayon_commun_entrenoeuds, coef_delta_feuilles, coef_delta_bourgeon, volume_fixe_feuilles, volume_fixe_bourgeon, v_1, k_1, v_2, k_2) :
 
-    C1_m = FLUX_2puits (Ci_m, C_0, longueur_commune_entrenoeuds, rayon_commun_entrenoeuds)[0] - UTILISATION (coef_delta_feuilles, coef_delta_bourgeon, volume_fixe_feuilles, volume_fixe_bourgeon, valeurs_RER_bourgeon, valeurs_RER_feuilles)[0]
-    C2_m = FLUX_2puits (Ci_m, C_0, longueur_commune_entrenoeuds, rayon_commun_entrenoeuds)[1] - UTILISATION (coef_delta_feuilles, coef_delta_bourgeon, volume_fixe_feuilles, volume_fixe_bourgeon, valeurs_RER_bourgeon, valeurs_RER_feuilles)[1]
+    dCi_m_dt = C0_m*FLUX_2puits (C1_m, C2_m, C0_m, longueur_commune_entrenoeuds, rayon_commun_entrenoeuds) - UTILISATION (coef_delta_feuilles, coef_delta_bourgeon, volume_fixe_feuilles, volume_fixe_bourgeon, C1_m, C2_m, v_1, k_1, v_2, k_2)
 
-    return C1_m, C2_m
+    return dCi_m_dt
 #### FONCTIONS
 
 # condition[0] = HH
@@ -145,7 +123,7 @@ def A_RESOUDRE (Ci_m, C_0, longueur_commune_entrenoeuds, rayon_commun_entrenoeud
 # condition[2] = LL
 
 print("DEBUT LOOP CONDITION _________________________")
-for condition in range(3) : 
+for condition, nom_condition in enumerate(nom_conditions) : 
     longueur_commune_entrenoeuds = BASE_longueur_commune_entrenoeuds[condition]
     rayon_commun_entrenoeuds = BASE_rayon_commun_entrenoeuds[condition]
     valeur_photosynthese = BASE_valeur_photosynthese[condition]
@@ -157,15 +135,55 @@ for condition in range(3) :
     volume_fixe_bourgeon = BASE_volume_fixe_bourgeon[condition]
     valeurs_RER_bourgeon = BASE_valeurs_RER_bourgeon[condition]
     valeurs_RER_feuilles = BASE_valeurs_RER_feuilles[condition]
+    coef_delta_feuilles = BASE_coef_delta_feuilles[condition]
+    coef_delta_bourgeon = BASE_coef_delta_bourgeon [condition]
+    k_1 = BASE_k1[condition]
+    v_1 = BASE_v1[condition]
+    k_2 = BASE_k2[condition]
+    v_2 = BASE_v2[condition]
 
-    increment_C1 = np.array([0, 2, 4], dtype=int)
-    increment_C2 = np.array([1, 3, 5], dtype=int)
-    conditions_ini_C1C2 = np.append(BASE_conditions_ini_C1C2[increment_C1[condition]], BASE_conditions_ini_C1C2[increment_C2[condition]])
-    print("PRINT INCREMENT C1 :", BASE_conditions_ini_C1C2[increment_C1[condition]])
-    print("PRINT INCREMENT C2 :", BASE_conditions_ini_C1C2[increment_C2[condition]])
+    C1_m_t0 = BASE_conc_ini_C1[condition]
+    C2_m_t0 = BASE_conc_ini_C2[condition]
 
-    nom_condition = np.array(["HH", "LH", "LL"], dtype=str)
+    conditions_ini_C1C2 = np.append(C1_m_t0[condition], C2_m_t0[condition])
 
-    Ci_m = scipy.fsolve(A_RESOUDRE, x0=conditions_ini_C1C2, args=(concentration_0, longueur_commune_entrenoeuds, rayon_commun_entrenoeuds, coef_delta_feuilles, coef_delta_bourgeon, volume_fixe_feuilles, volume_fixe_bourgeon, valeurs_RER_bourgeon, valeurs_RER_feuilles), col_deriv=0, xtol=1.49012e-08, maxfev=0, band=None, epsfcn=None, factor=100, diag=None) 
-    print("Les solutions à l'équilibre C1_m et C2_m pour la condition",nom_condition[condition], "sont :", Ci_m)
+    # increment_C1 = np.array([0, 2, 4], dtype=int)
+    # increment_C2 = np.array([1, 3, 5], dtype=int)
+    # conditions_ini_C1C2 = np.append(BASE_conditions_ini_C1C2[increment_C1[condition]], BASE_conditions_ini_C1C2[increment_C2[condition]])
+    # print("PRINT INCREMENT C1 :", BASE_conditions_ini_C1C2[increment_C1[condition]])
+    # print("PRINT INCREMENT C2 :", BASE_conditions_ini_C1C2[increment_C2[condition]])
+
+    dCi_m_dt = scipy.fsolve(A_RESOUDRE, x0=conditions_ini_C1C2, args=(concentration_0, longueur_commune_entrenoeuds, rayon_commun_entrenoeuds, coef_delta_feuilles, coef_delta_bourgeon, volume_fixe_feuilles, volume_fixe_bourgeon, v_1, k_1, v_2, k_2), col_deriv=0, xtol=1.49012e-08, maxfev=0, band=None, epsfcn=None, factor=100, diag=None) 
+    print("Les solutions à l'équilibre C1_m et C2_m pour la condition",nom_condition[condition], "sont :", dCi_m_dt)
+
 print("FIN LOOP CONDITION _________________________")
+
+
+
+
+# #### PLOTTING
+# fig,ax = plt.subplots(2)
+
+
+# # ax[0].plot(range(3), C0_m_equilibre , color="blue", marker="+")
+# ax[0].plot(range(3), Ci_m[increment_C1], color="red", marker="+")
+
+# ax[0].set_title("C1_m à l'équilibre")
+# ax[0].set_xlabel("HH LH LL", color="black", fontsize=14)
+# ax[0].set_ylabel("Concentration en umolEqGlucose/gMS", color="black", fontsize=14)
+
+# # location = 0
+# # legend_drawn_flag = True
+# # ax[0].legend(["C1_m", "C2_m"], loc=0, frameon=legend_drawn_flag)
+
+# ax[0].plot(range(3), Ci_m[increment_C2], color="yellow", marker="+")
+# ax[0].set_xlabel("HH LH LL", color="black", fontsize=14)
+# ax[0].set_ylabel("Concentration en umolEqGlucose/gMS", color="black", fontsize=14)
+#     #### PLOTTING
+
+
+
+# plt.show()
+
+
+
